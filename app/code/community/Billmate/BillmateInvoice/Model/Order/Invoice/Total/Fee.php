8f8b5d3aafc0b97b36dcb5aa49c3c995410d3cd4
jesper@boxedlogistics.se <emjesper@boxedlogistics.se/em>
2014-12-09 13:20:36 +0100

    Created a feature regarding invoicing and creditmemo in mageno

---
+++ app/code/community/Billmate/BillmateInvoice/Model/Order/Invoice/Total/Fee.php
@@ -0,0 +1,25 @@
+<?php
+class Billmate_BillmateInvoice_Model_Order_Invoice_Total_Fee extends Mage_Sales_Model_Order_Total_Abstract
+{
+    public function collect(Mage_Sales_Model_Order_Invoice $invoice){
+        $order = $invoice->getOrder();
+        $feeAmountLeft = $order->getFeeAmount() - $order->getFeeAmountInvoiced();
+        $baseFeeAmountLeft = $order->getBaseFeeAmount() - $order->getBaseFeeAmountInvoiced();
+
+        if(abs($baseFeeAmountLeft) < $invoice->getBaseGrandTotal()){
+            $invoice->setGrandTotal($invoice->getGrandTotal() + $baseFeeAmountLeft);
+            $invoice->setBaseGrandTotal($invoice->getBaseGrandTotal() + $baseFeeAmountLeft);
+        } else {
+            $feeAmountLeft = $invoice->getGrandTotal() * -1;
+            $baseFeeAmountLeft = $invoice->getBaseGrandTotal() * -1;
+
+            $invoice->setGrandTotal(0);
+            $invoice->setBaseGrandTotal(0);
+        }
+        $invoice->setFeeAmount($feeAmountLeft);
+        $invoice->setBaseFeeAmount($baseFeeAmountLeft);
+
+        return $this;
+    }
+
+}
--- app/code/community/Billmate/BillmateInvoice/sql/billmateinvoice_setup/mysql4-upgrade-1.0.0-1.3.1.php
+++ app/code/community/Billmate/BillmateInvoice/sql/billmateinvoice_setup/mysql4-upgrade-1.0.0-1.3.1.php
@@ -1,9 +1,8 @@
 <?php
 echo 'Running This Upgrade: '.get_class($this)."\n <br /> \n";
 $installer = $this;
-/* @var $installer Mage_Catalog_Model_Entity_Setup */
+/* @var $installer Mage_Sales_Model_Resource_Setup */
 $installer->startSetup();
-$installer->run("ALTER TABLE  `".$this->getTable('sales/order')."` ADD  `fee_amount` DECIMAL( 10, 2 ) NOT NULL;");
-$installer->run("ALTER TABLE  `".$this->getTable('sales/order')."` ADD  `base_fee_amount` DECIMAL( 10, 2 ) NOT NULL;");
+$installer->addAttribute('order','fee_amount',array('type' => Varien_Db_Ddl_Table::TYPE_DECIMAL));
+$installer->addAttribute('order','base_fee_amount',array('type' => Varien_Db_Ddl_Table::TYPE_DECIMAL));
 $installer->endSetup();
-die("Exit for now");
