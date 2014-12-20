5ef9ad2b7b4730b3813fa4175fbee3504d2d6db3
Jesper Johansson <jesper@boxedlogistics.se>
2014-12-20 11:06:08 +0100

    Some typos
    Corrected to right Billmate.php not BillMate.php

--- app/code/community/Billmate/Cardpay/Helper/Data.php
+++ app/code/community/Billmate/Cardpay/Helper/Data.php
@@ -3,7 +3,7 @@ class Billmate_Cardpay_Helper_Data extends Mage_Core_Helper_Abstract
 {
     function getBillmate($ssl = true, $debug = false ){
 
-        require_once Mage::getBaseDir('lib').'/Billmate/BillMate.php';
+        require_once Mage::getBaseDir('lib').'/Billmate/Billmate.php';
         require_once Mage::getBaseDir('lib').'/Billmate/utf8.php';
         include_once(Mage::getBaseDir('lib')."/Billmate/xmlrpc-2.2.2/xmlrpc.inc");
         include_once(Mage::getBaseDir('lib')."/Billmate/xmlrpc-2.2.2/xmlrpcs.inc");
