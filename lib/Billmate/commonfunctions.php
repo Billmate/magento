9f9fb678fe5ea9b59a40941f5c2cd6fe2c143db0
Jesper Johansson <jesper@boxedlogistics.se>
2015-01-16 14:15:02 +0100

    Changed som eFinance to Billmate

--- lib/Billmate/commonfunctions.php
+++ lib/Billmate/commonfunctions.php
@@ -1,6 +1,6 @@
 <?php
 if(!function_exists('getCountryID')){
-	define('BILLPLUGIN_VERSION', '1.25');
+	define('BILLPLUGIN_VERSION', '2.0');
 	define('BILLMATE_VERSION',  "PHP:Magento:".BILLPLUGIN_VERSION );
 
 	function getCountryID(){
