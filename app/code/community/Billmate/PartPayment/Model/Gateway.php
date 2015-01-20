b361458007bcc8954b03caf1c6da0a2d894f9857
Jesper Johansson <jesper@boxedlogistics.se>
2015-01-20 13:14:31 +0100

    Discount Rounding issue solved
    Fixes #41

--- app/code/community/Billmate/PartPayment/Model/Gateway.php
+++ app/code/community/Billmate/PartPayment/Model/Gateway.php
@@ -245,18 +245,23 @@ class Billmate_Partpayment_Model_Gateway extends Varien_Object{
         $totals = Mage::getSingleton('checkout/session')->getQuote()->getTotals();
 
         if(isset($totals['discount']) && !$discountAdded) {
+
+            $floor = 1+($percent/100);
+            $marginal = 1 / $floor;
+            $price = $totals['discount']->getValue() * $marginal;
+
             $orderValues['Articles'][] = array(
                 'quantity'   => (int)1,
                 'artnr'    => 'discount',
                 'title'    => Mage::helper('payment')->__('Discount'),
-                'aprice'    => round($totals['discount']->getValue()*0.8)*100,
+                'aprice'    => round(($totals['discount']->getValue()*$marginal)*100),
                 'taxrate'      => (float)$percent,
                 'discount' => 0.0,
-                'withouttax'    => round($totals['discount']->getValue()*0.8)*100,
+                'withouttax'    => round(($totals['discount']->getValue()*$marginal)*100),
 
             );
-            $totalValue += (1 * round($totals['discount']->getValue()*0.8))*100;
-            $totalTax += ((1 * round($totals['discount']->getValue()*0.8))*100) * ($percent/100);
+            $totalValue += (1 * round(($totals['discount']->getValue()* $marginal)*100));
+            $totalTax += (1 * round(($totals['discount']->getValue()*$marginal)*100) * ($percent/100));
         }
         /*
         if(isset($totals['discount']) && $discountAdded) {
