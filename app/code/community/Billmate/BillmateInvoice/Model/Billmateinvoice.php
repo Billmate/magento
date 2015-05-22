9ff986acbcc66b284e32cfc7b8bec6a36a577b6c
jesper@boxedlogistics.se <jesper@boxedlogistics.se>
2015-05-22 17:41:39 +0200

    Fixes invoice fee in title and GetAddress for companies
    Fixes #108 and Fixes #110

--- app/code/community/Billmate/BillmateInvoice/Model/Billmateinvoice.php
+++ app/code/community/Billmate/BillmateInvoice/Model/Billmateinvoice.php
@@ -81,7 +81,19 @@ class Billmate_BillmateInvoice_Model_BillmateInvoice extends Mage_Payment_Model_
 	    $payment->setIsTransactionClosed(0);
     }
     public function getTitle(){
-        return (strlen(Mage::getStoreConfig('payment/billmateinvoice/title')) > 0) ? Mage::helper('billmateinvoice')->__(Mage::getStoreConfig('payment/billmateinvoice/title')) : Mage::helper('billmateinvoice')->__('Billmate Invoice');
+
+        $invoiceFee = Mage::getStoreConfig( 'payment/billmateinvoice/billmate_fee' );
+        $invoiceFee = Mage::helper( 'billmateinvoice' )->replaceSeparator( $invoiceFee );
+
+        $quote = Mage::getModel('checkout/cart')->getQuote();
+        $Shipping = $quote->getShippingAddress();
+        $feeinfo = Mage::helper( 'billmateinvoice' )
+            ->getInvoiceFeeArray( $invoiceFee, $Shipping, $quote->getCustomerTaxClassId() );
+
+        $invFee = (isset($feeinfo['rate']) && $feeinfo['rate'] != 0) ? ($feeinfo['rate']/100 + 1) * $invoiceFee : $invoiceFee;
+
+        $invFee = Mage::helper('core')->currency($invFee, true, false);
+        return (strlen(Mage::getStoreConfig('payment/billmateinvoice/title')) > 0) ? Mage::helper('billmateinvoice')->__(Mage::getStoreConfig('payment/billmateinvoice/title'),$invFee) : Mage::helper('billmateinvoice')->__('Billmate Invoice - %s invoice fee is added to the order sum',$invFee);
     }
 
     public function capture(Varien_Object $payment, $amount)
