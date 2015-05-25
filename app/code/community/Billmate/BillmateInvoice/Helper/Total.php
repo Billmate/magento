3a11994b0309eb97aa675e389de6c27caf54e86f
jesper@boxedlogistics.se <jesper@boxedlogistics.se>
2015-05-25 14:31:19 +0200

    Fix for if tax is set and not shown correct in backend

--- app/code/community/Billmate/BillmateInvoice/Helper/Total.php
+++ app/code/community/Billmate/BillmateInvoice/Helper/Total.php
@@ -12,7 +12,11 @@ class Billmate_BillmateInvoice_Helper_Total extends Mage_Core_Helper_Abstract
         $vatOption = Mage::getStoreConfig("tax/sales_display/price", $storeId);
 
         $invoiceFee = $info->getAdditionalInformation('billmateinvoice_fee');
-
+        $invoiceTax = $info->getAdditionalInformation('billmateinvoice_fee_tax');
+        $origInvFee = Mage::getStoreConfig('payment/billmateinvoice/billmate_fee');
+        if($invoiceTax != 0 && ($invoiceFee == $origInvFee)){
+            $invoiceFee += $invoiceTax;
+        }
 		$extra = '';
 		
         $fee = new Varien_Object();
