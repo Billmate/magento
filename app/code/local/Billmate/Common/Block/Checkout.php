206cd049c51e000eb84e3fcf73cdaa994c7aedfb
Boxedsolutions <jesper@boxedlogistics.se>
2017-02-07 11:42:16 +0100

    Persist the checkout when reload the window.
    Fixes #177

--- app/code/local/Billmate/Common/Block/Checkout.php
+++ app/code/local/Billmate/Common/Block/Checkout.php
@@ -10,11 +10,21 @@ class Billmate_Common_Block_Checkout extends Mage_Core_Block_Template
 {
     public function getCheckoutUrl()
     {
-        $checkout = Mage::getModel('billmatecommon/checkout')->init();
-        Mage::getSingleton('checkout/session')->setBillmateInvoiceId($checkout['number']);
-        Mage::log('checkout'.print_r($checkout,true));
-        if(!isset($checkout['code'])){
-            return $checkout['url'];
+        if(Mage::getSingleton('checkout/session')->getBillmateHash()){
+            $billmate = Mage::helper('billmatecommon')->getBillmate();
+            $checkout = $billmate->getCheckout(array('PaymentData' => array('hash' => Mage::getSingleton('checkout/session')->getBillmateHash())));
+            if(!isset($checkout['code'])){
+                return $checkout['PaymentData']['url'];
+            }
+
+        } else {
+            $checkout = Mage::getModel('billmatecommon/checkout')->init();
+            Mage::getSingleton('checkout/session')->setBillmateInvoiceId($checkout['number']);
+            Mage::log('checkout'.print_r($checkout,true));
+            if(!isset($checkout['code'])){
+                return $checkout['url'];
+            }
         }
+
     }
 }
