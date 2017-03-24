c6397363d56813516440babed908c1611671ca45
Boxedsolutions <jesper@boxedlogistics.se>
2017-03-24 15:58:17 +0100

    Changed format for url for accept urls to fix redirect to thankyou page

--- app/code/local/Billmate/Common/Model/Checkout.php
+++ app/code/local/Billmate/Common/Model/Checkout.php
@@ -315,16 +315,16 @@ class Billmate_Common_Model_Checkout extends Varien_Object
         $orderValues['PaymentData']['orderid'] = $quote->getReservedOrderId();
         //$orderValues['PaymentData']['method'] = $method;
         if(8 == $orderValues['PaymentData']['method']) {
-            $orderValues['PaymentData']['accepturl'] = Mage::getUrl('cardpay/cardpay/accept', array('billmate_checkout' => true,'billmate_quote_id' => $quote->getId(), '_secure' => true));
+            $orderValues['PaymentData']['accepturl'] = Mage::getUrl('cardpay/cardpay/accept', array('_query' => array('billmate_checkout' => true,'billmate_quote_id' => $quote->getId()), '_secure' => true));
             $orderValues['PaymentData']['cancelurl'] = Mage::getUrl('cardpay/cardpay/cancel', array('_secure' => true));
-            $orderValues['PaymentData']['callbackurl'] = Mage::getUrl('cardpay/cardpay/callback', array('billmate_quote_id' => $quote->getId(), '_secure' => true));
+            $orderValues['PaymentData']['callbackurl'] = Mage::getUrl('cardpay/cardpay/callback', array('_query' => array('billmate_quote_id' => $quote->getId()), '_secure' => true));
             $orderValues['PaymentData']['returnmethod'] = (Mage::app()->getStore()->isCurrentlySecure()) ? 'POST' : 'GET';
         }
 
         if(16 == $orderValues['PaymentData']['method']) {
-            $orderValues['PaymentData']['accepturl'] = Mage::getUrl('bankpay/bankpay/accept', array('billmate_checkout' => true,'billmate_quote_id' => $quote->getId(), '_secure' => true));
+            $orderValues['PaymentData']['accepturl'] = Mage::getUrl('bankpay/bankpay/accept', array('_query' => array('billmate_checkout' => true,'billmate_quote_id' => $quote->getId()), '_secure' => true));
             $orderValues['PaymentData']['cancelurl'] = Mage::getUrl('bankpay/bankpay/cancel', array('_secure' => true));
-            $orderValues['PaymentData']['callbackurl'] = Mage::getUrl('bankpay/bankpay/callback', array('billmate_quote_id' => $quote->getId(), '_secure' => true));
+            $orderValues['PaymentData']['callbackurl'] = Mage::getUrl('bankpay/bankpay/callback', array('_query' => array('billmate_quote_id' => $quote->getId()), '_secure' => true));
             $orderValues['PaymentData']['returnmethod'] = (Mage::app()->getStore()->isCurrentlySecure()) ? 'POST' : 'GET';
         }
         $_taxHelper  = Mage::helper('tax');
