9655a764103861d1932d725985dc305fb25c8f6d
Boxedsolutions <jesper@boxedlogistics.se>
2017-04-10 16:55:16 +0200

    Corrected a logic thing

--- app/code/community/Billmate/PartPayment/Block/Payfrom.php
+++ app/code/community/Billmate/PartPayment/Block/Payfrom.php
@@ -12,8 +12,7 @@ class Billmate_PartPayment_Block_Payfrom extends Mage_Core_Block_Template
     {
         if(!Mage::getStoreConfig('billmate/settings/show_payfrom',Mage::app()->getStore()->getId()))
             return false;
-        if(!Mage::helper('nmc_tecdoc')->isPriceVisible(Mage::registry('product')))
-            return false;
+
         if(($product = Mage::registry('product'))){
             if(!Mage::getSingleton('customer/session')->isLoggedIn()) {
                 return $this->helper('partpayment')->getLowPclass($product->getPrice(), true);
