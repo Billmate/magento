b57a9c729ba0b52ae5f28b427a8b1c5e3238a63f
alex <a.grigorenko.belvg@gmail.com>
2019-03-26 17:49:14 +0300

    remove install scripts from merge method

--- app/code/community/Billmate/BillmateCheckout/sql/billmateinvoice_setup/mysql4-upgrade-1.0.0-1.3.1.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo 'Running This Upgrade: '.get_class($this)."\n <br /> \n";
-$installer = $this;
-/* @var $installer Mage_Sales_Model_Resource_Setup */
-$installer->startSetup();
-$installer->addAttribute('order','fee_amount',array('type' => Varien_Db_Ddl_Table::TYPE_DECIMAL));
-$installer->addAttribute('order','base_fee_amount',array('type' => Varien_Db_Ddl_Table::TYPE_DECIMAL));
-$installer->endSetup();
